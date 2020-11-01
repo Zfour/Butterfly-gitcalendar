cb4835f89826b9b0c09c5f49b5c6338b7d986420
Zfour <499984532@qq.com>
2020-11-02 00:29:57 +0800

    将部分全局声明变量转换为了vue实例变量，关闭了console.log

--- gitcalendar/js/gitcalendar.js
+++ gitcalendar/js/gitcalendar.js
@@ -36,6 +36,11 @@ const calendar = new Vue({
             mounthbeforeday:0,
             mounthfirstindex:0,
             crispedges:'crispedges',
+			thisdayindex:0,
+			amonthagoindex:0,
+			amonthagoweek:[],
+			firstdate:[],
+			first2date:[],
             purple: [
             '#ebedf0',
             '#fdcdec',
@@ -77,16 +82,16 @@ const calendar = new Vue({
 			 ],
              color: [ //这里是颜色配置，当前为蓝色，若需更换请对应数组内容进行替换
             '#ebedf0',
-            '#f0fff4',
-            '#dcffe4',
-            '#bef5cb',
-            '#85e89d',
-            '#34d058',
-            '#28a745',
-            '#22863a',
-            '#176f2c',
-            '#165c26',
-            '#144620'
+            '#fdcdec',
+            '#fc9bd9',
+            '#fa6ac5',
+            '#f838b2',
+            '#f5089f',
+            '#c4067e',
+            '#92055e',
+            '#540336',
+            '#48022f',
+            '#30021f',
              ]
         },
         methods:{
@@ -96,11 +101,11 @@ const calendar = new Vue({
                 this.span2 = data.count;
                 this.x = event.clientX -100;
                 this.y = event.clientY -60;
-                console.log(event);
+                //console.log(event);
             },
             outStyle(){
                 $('.angle-wrapper').hide();
-                console.log();
+                //console.log();
             },
             thiscolor(x){
                 if(x===0){
@@ -139,77 +144,77 @@ const calendar = new Vue({
             },
             //请求成功后要执行的函数，拼接html
             success: function (data) {
-                console.log(data);
+                //console.log(data);
                 calendar.data = data.contributions;
                 calendar.total = data.total;
                 calendar.firstweek= data.contributions[0];
                 calendar.lastweek= data.contributions[52];
                 calendar.beforeweek= data.contributions[51];
-                thisdayindex = calendar.lastweek.length - 1;
-                calendar.thisday = calendar.lastweek[thisdayindex].date;
+                calendar.thisdayindex = calendar.lastweek.length - 1;
+                calendar.thisday = calendar.lastweek[calendar.thisdayindex].date;
                 calendar.oneyearbeforeday = calendar.firstweek[0].date;
-                amonthagoindex =  6 - (30-calendar.lastweek.length)%7;
+                calendar.amonthagoindex =  6 - (30-calendar.lastweek.length)%7;
                 calendar.mounthfirstindex = 52 - Math.ceil((30- calendar.lastweek.length*1)/7);
-                amonthagoweek =  calendar.data[calendar.mounthfirstindex];
-                calendar.amonthago = amonthagoweek[amonthagoindex].date;
+                calendar.amonthagoweek =  calendar.data[calendar.mounthfirstindex];
+                calendar.amonthago = calendar.amonthagoweek[calendar.amonthagoindex].date;
                 addlastmonth();
 				reloadmonth(calendar.thisday);
                 function addlastmonth(){
-                    if(thisdayindex === 0){
+                    if(calendar.thisdayindex === 0){
                         thisweekcore(52);
                         thisweekcore(51);
                         thisweekcore(50);
                         thisweekcore(49);
                         thisweekcore(48);
-                        let firstdate = calendar.data[47]
-                        calendar.thisweekdatacore += firstdate[6].count
-                        console.log('月'+ firstdate[6].date);
+                        calendar.firstdate = calendar.data[47]
+                        calendar.thisweekdatacore += calendar.firstdate[6].count
+                        //console.log('月'+ calendar.firstdate[6].date);
                     }
                     else {
                         thisweekcore(52);
                         thisweekcore(51);
                         thisweekcore(50);
                         thisweekcore(49);
-                         first2date = calendar.data[48]
+                        calendar.first2date = calendar.data[48]
                         thisweek2core();
                     }
 
                 };
-                function thisweek2core(){for( let i=thisdayindex-2;i<first2date.length;i++){
+                function thisweek2core(){for( let i=calendar.thisdayindex-2;i<calendar.first2date.length;i++){
 
-                    calendar.thisweekdatacore += first2date[i].count*1;
-                    console.log('月'+ first2date[i].date);
+                    calendar.thisweekdatacore += calendar.first2date[i].count*1;
+                    //console.log('月'+ calendar.first2date[i].date);
 
                 }};
                 function thisweekcore(index){for( let item of calendar.data[index]){
 
                     calendar.thisweekdatacore += item.count*1;
-                    console.log('月'+ item.date);
+                    //console.log('月'+ item.date);
 
                 }};
                 function addlastweek(){for( let item of calendar.lastweek){
 
                     calendar.weekdatacore += item.count*1;
-                    console.log('日'+ item.date);
+                    //console.log('日'+ item.date);
 
                 }};
 				function reloadmonth(thisday){
                   let  str = thisday.substring(5, 7);
                      let arr = calendar.month.splice(str,12-str*1);
-                     console.log(arr);
+                     //console.log(arr);
                     calendar.monthchange = arr.concat(calendar.month)
                 };
-                function addbeforeweek(){for( let i=thisdayindex;i<calendar.beforeweek.length;i++){
+                function addbeforeweek(){for( let i=calendar.thisdayindex;i<calendar.beforeweek.length;i++){
                     calendar.weekdatacore += calendar.beforeweek[i].count*1;
-                    console.log('日'+ calendar.beforeweek[i].date);
+                    //console.log('日'+ calendar.beforeweek[i].date);
 
                 }};
-                if(thisdayindex === 6 ){
+                if(calendar.thisdayindex === 6 ){
                     calendar.aweekago  = calendar.lastweek[0].date;
                     addlastweek();
                 }else{
                     lastweek = data.contributions[51];
-                    calendar.aweekago  = lastweek[thisdayindex].date;
+                    calendar.aweekago  = lastweek[calendar.thisdayindex].date;
                     addlastweek();
                     addbeforeweek();
                 };
